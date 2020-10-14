d30e243c0f50b1855a34d0fd2a1766284dc62861
Julian Salgado <61922334+josesalgado-15@users.noreply.github.com>
2020-10-14 15:36:35 -0500

    * Migracion de Departamentos, Municipios, empresa, sucursal. * Creacion de enums

--- database/migrations/2020_10_14_164315_create_sucursal_table.php
+++ database/migrations/2020_10_14_164315_create_sucursal_table.php
@@ -15,7 +15,35 @@ class CreateSucursalTable extends Migration
     {
         Schema::create('sucursal', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre', 60)->index();
+            $table->unsignedBigInteger('municipio_id')->index()->unsigned(); //llave foranea
+            $table->string('direccion', 60);
+            $table->bigInteger('telefono')->unsigned();
+
+            $table->unsignedBigInteger('jefe_id')->nullable()->unsigned();//llave foranea opcional
+
+
+            $table->unsignedBigInteger('empresa_id')->index()->unsigned(); //llave foranea
+
             $table->timestamps();
+
+
+            $table->foreign('empresa_id')
+                ->references('id')->on('empresas')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('municipio_id')
+                ->references('id')->on('municipio')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('jefe_id')
+                ->references('id')->on('usuario')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->softDeletes();
         });
     }
 
